cef66fdda4b1defa1133bd2b35ba40cf1981b6ea
Yann Papouin <ypa@decgroupe.com>
2024-04-17 17:23:25 +0200

    [IMP] email_template_qweb: Apply directly on field rendering
    Also use a common qweb eval context that can easily be extended

--- email_template_qweb/models/mail_template.py
+++ email_template_qweb/models/mail_template.py
@@ -15,33 +15,58 @@ class MailTemplate(models.Model):
     body_view_id = fields.Many2one("ir.ui.view", domain=[("type", "=", "qweb")])
     body_view_arch = fields.Text(related="body_view_id.arch")
 
-    def generate_email(self, res_ids, fields):
-        multi_mode = True
-        if isinstance(res_ids, int):
-            res_ids = [res_ids]
-            multi_mode = False
-        result = super(MailTemplate, self).generate_email(res_ids, fields=fields)
-        for lang, (_template, _template_res_ids) in self._classify_per_lang(
-            res_ids
-        ).items():
-            self_with_lang = self.with_context(lang=lang)
-            for res_id in res_ids:
-                if self.body_type == "qweb" and (not fields or "body_html" in fields):
-                    for record in self_with_lang.env[self.model].browse(res_id):
-                        body_html = self_with_lang.body_view_id._render(
-                            {"object": record, "email_template": self_with_lang}
-                        )
-                        # Some wizards, like when sending a sales order, need this
-                        # fix to display accents correctly
-                        body_html = tools.ustr(body_html)
-                        result[res_id][
-                            "body_html"
-                        ] = self_with_lang._render_template_postprocess(
-                            {res_id: body_html}
-                        )[
-                            res_id
-                        ]
-                        result[res_id]["body"] = tools.html_sanitize(
-                            result[res_id]["body_html"]
-                        )
-        return result if multi_mode else result[res_ids[0]]
+    def _render_qweb_body_eval_context(self, record):
+        render_context = self._render_qweb_eval_context()
+        render_context.update(
+            {
+                "object": record,
+                "email_template": self,
+                "company": self.env.user.company_id,
+            }
+        )
+        return render_context
+
+    def _render_qweb_body(
+        self, res_ids, compute_lang=False, set_lang=False, post_process=False
+    ):
+        self.ensure_one()
+        if compute_lang:
+            templates_res_ids = self._classify_per_lang(res_ids)
+        elif set_lang:
+            templates_res_ids = {set_lang: (self.with_context(lang=set_lang), res_ids)}
+        else:
+            templates_res_ids = {self._context.get("lang"): (self, res_ids)}
+        res = {}
+        rendered_templates = {}
+        for lang, (template, tpl_res_ids) in templates_res_ids.items():
+            for res_id in tpl_res_ids:
+                record = template.env[template.model].browse(res_id)
+                values = template._render_qweb_body_eval_context(record)
+                rendered_templates[res_id] = template.body_view_id._render(values)
+            if post_process:
+                rendered_templates = self._render_template_postprocess(
+                    rendered_templates
+                )
+            for res_id, rendered in rendered_templates.items():
+                res[res_id] = rendered
+        return res
+
+    def _render_field(
+        self, field, res_ids, compute_lang=False, set_lang=False, post_process=False
+    ):
+        if self.body_type == "qweb" and field == "body_html":
+            res = self._render_qweb_body(
+                res_ids,
+                compute_lang=compute_lang,
+                set_lang=set_lang,
+                post_process=post_process,
+            )
+        else:
+            res = super()._render_field(
+                field=field,
+                res_ids=res_ids,
+                compute_lang=compute_lang,
+                set_lang=set_lang,
+                post_process=post_process,
+            )
+        return res
